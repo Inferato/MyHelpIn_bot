60ff1c133e952b128726550b7bb25da936955d53
Inferato <Infaustmager@gmail.com>
2020-09-07 00:57:33 +0300

    fix

--- main.py
+++ main.py
@@ -28,10 +28,7 @@ class BotHandler:
 
         if len(get_result) > 0:
             last_update = get_result[-1]
-        else:
-            last_update = get_result[len(get_result)]
-
-        return last_update
+            return last_update
 
 
 greet_bot = BotHandler(token)
@@ -48,41 +45,41 @@ def main():
         greet_bot.get_updates(new_offset)
 
         last_update = greet_bot.get_last_update()
+        if last_update:
+            last_update_id = last_update['update_id']
+            last_chat_text = last_update['message']['text']
+            last_chat_id = last_update['message']['chat']['id']
+            last_chat_name = last_update['message']['chat']['first_name']
 
-        last_update_id = last_update['update_id']
-        last_chat_text = last_update['message']['text']
-        last_chat_id = last_update['message']['chat']['id']
-        last_chat_name = last_update['message']['chat']['first_name']
+            if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+                greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+                # today += 1
 
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-            # today += 1
+            elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+                greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+                # today += 1
 
-        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-            # today += 1
+            elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+                greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+                # today += 1
 
-        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-            # today += 1
-
-        elif last_chat_text.lower() == "погода":
-            resp = getweather()
-            descpiption = resp['weather'][0]['description']
-            temp = resp['main']['temp']
-            feels_like = resp['main']['feels_like']
-            temp_min = resp['main']['temp_min']
-            temp_max = resp['main']['temp_max']
-            name = resp['name']
-            weather = "Выбранный город:  {}".format(name) + \
-                      "\n " + descpiption + "\n температура: {}С".format(temp) + \
-                "\n чувствуется как: {}C".format(feels_like) + \
-                "\n минимальная температура: {}C".format(temp_min) + \
-                "\n максимальная температура: {}C".format(temp_max)
-            greet_bot.send_message(last_chat_id, 'Прогноз погоды на сегодня: \n {}'.format(weather))
+            elif last_chat_text.lower() == "погода":
+                resp = getweather()
+                descpiption = resp['weather'][0]['description']
+                temp = resp['main']['temp']
+                feels_like = resp['main']['feels_like']
+                temp_min = resp['main']['temp_min']
+                temp_max = resp['main']['temp_max']
+                name = resp['name']
+                weather = "Выбранный город:  {}".format(name) + \
+                          "\n " + descpiption + "\n температура: {}С".format(temp) + \
+                    "\n чувствуется как: {}C".format(feels_like) + \
+                    "\n минимальная температура: {}C".format(temp_min) + \
+                    "\n максимальная температура: {}C".format(temp_max)
+                greet_bot.send_message(last_chat_id, 'Прогноз погоды на сегодня: \n {}'.format(weather))
 
 
-        new_offset = last_update_id + 1
+            new_offset = last_update_id + 1
 
 
 if __name__ == '__main__':
