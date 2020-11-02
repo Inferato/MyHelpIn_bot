6b1101b43448a800be83b0caaf0364215f2bf367
Inferato <Infaustmager@gmail.com>
2020-11-02 03:25:00 +0200

    add city choose

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import requests
 import datetime
 from wethr import getweather
-token = "1037730614:AAG4BR2gc9b157zZhbxVUYhtmg55oaAindU"
+from transliterate import translit, get_available_language_codes
+from googletrans import Translator
 
+token = "1037730614:AAG4BR2gc9b157zZhbxVUYhtmg55oaAindU"
+translator = Translator()
 
 class BotHandler:
 
@@ -36,7 +39,6 @@ class BotHandler:
 
 
 greet_bot = BotHandler(token)
-greetings = ('здравствуй', 'привет', 'ку', 'здорово')
 now = datetime.datetime.now()
 
 
@@ -56,32 +58,32 @@ def main():
             last_chat_id = last_update['message']['chat']['id']
             last_chat_name = last_update['message']['chat']['first_name']
 
-            if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-                greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-                # today += 1
-
-            elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-                greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-                # today += 1
-
-            elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-                greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-                # today += 1
-
-            elif last_chat_text.lower() == "погода":
-                resp = getweather()
-                descpiption = resp['weather'][0]['description']
-                temp = resp['main']['temp']
-                feels_like = resp['main']['feels_like']
-                temp_min = resp['main']['temp_min']
-                temp_max = resp['main']['temp_max']
-                name = resp['name']
-                weather = "Выбранный город:  {}".format(name) + \
-                          "\n " + descpiption + "\n температура: {}С".format(temp) + \
-                    "\n чувствуется как: {}C".format(feels_like) + \
-                    "\n минимальная температура: {}C".format(temp_min) + \
-                    "\n максимальная температура: {}C".format(temp_max)
-                greet_bot.send_message(last_chat_id, 'Прогноз погоды на сейчас: \n {}'.format(weather))
+            if last_chat_text.lower() == "погода":
+                greet_bot.send_message(last_chat_id, 'Укажите Ваш город: ')
+                new_offset = last_update_id + 1
+                greet_bot.deletewebhook()
+                greet_bot.get_updates(new_offset)
+                last_update = greet_bot.get_last_update()
+                if last_update:
+                    last_chat_text = last_update['message']['text']
+                    city_translate = str(translator.translate(last_chat_text.capitalize(), 'uk', 'ru').text)
+                    city = str(translator.translate(city_translate, 'en', 'uk').text)
+                    try:
+                        resp = getweather(city)
+                        descpiption = resp['weather'][0]['description']
+                        temp = resp['main']['temp']
+                        feels_like = resp['main']['feels_like']
+                        temp_min = resp['main']['temp_min']
+                        temp_max = resp['main']['temp_max']
+                        name = resp['name']
+                        weather = "Выбранный город:  {}".format(name) + \
+                                  "\n " + descpiption + "\n температура: {}С".format(temp) + \
+                            "\n чувствуется как: {}C".format(feels_like) + \
+                            "\n минимальная температура: {}C".format(temp_min) + \
+                            "\n максимальная температура: {}C".format(temp_max)
+                        greet_bot.send_message(last_chat_id, 'Прогноз погоды на сейчас: \n {}'.format(weather))
+                    except KeyError as e:
+                        greet_bot.send_message(last_chat_id, 'Прогноз погоды на сейчас: \n{}'.format(str(translator.translate(resp['message'], 'ru', 'en').text)))
 
             new_offset = last_update_id + 1
 
@@ -91,4 +93,3 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         exit()
-        main()
